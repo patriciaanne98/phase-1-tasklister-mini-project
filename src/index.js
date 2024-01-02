1fc6c9f3b5266a43e2f3864a60cc70512b1399bf
Patricia Frogoso <pfrogoso@gmail.com>
2024-01-02 11:13:04 -0600

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", (e) => {
+  const form = document.getElementById("create-task-form");
+
+  form.addEventListener("submit", (e)=> {
+    e.preventDefault();
+    const userInput = form.querySelector("#new-task-description").value;
+    console.log(userInput)
+    buildToDo(userInput)
+  })
   // your code here
 });
+
+function buildToDo(userInput) {
+  const ul = document.getElementById("tasks");
+  console.log(ul)
+  const li = document.createElement("li");
+  li.innerText = userInput
+  ul.appendChild(li)
+  console.log(li)
+}
+
+// Step 1: either grab or create html element
+// Step 2: do something with that html element
+// Step 3: append the element to the DOM
